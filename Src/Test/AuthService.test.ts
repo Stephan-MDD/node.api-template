98accf7717e934943569f91572cb940072058d66
Stephan <stephan.mdd@gmail.com>
2021-03-21 15:13:54 +0100

    added test fundamentals

---
+++ Src/Test/AuthService.test.ts
@@ -0,0 +1,5 @@
+import { AuthService } from '../Services';
+
+test('template', () => {
+	expect(1 + 2).toBe(3);
+});
---
+++ Src/Test/UserService.test.ts
@@ -0,0 +1,5 @@
+import { UserService } from '../Services';
+
+test('template', () => {
+	expect(1 + 2).toBe(3);
+});
