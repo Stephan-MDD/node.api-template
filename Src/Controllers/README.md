17db0b0f5d76bdd779654f3960ff587ea41795c4
Stephan <stephan.mdd@gmail.com>
2021-03-22 08:17:57 +0100

    optimized test suites

--- Src/Controllers/README.md
+++ Src/Controllers/README.md
@@ -3,3 +3,17 @@
 ## Controller
 
 About controller patterns
+
+```typescript
+// typescript
+router.post('/:id', authenticate(), async (req: Request, res: Response, next: NextFunction) => {
+	// get request date
+	const bodyData = res.body;
+	const paramData = res.params;
+	const queryData = res.query;
+
+	// apply service response to response object
+	res.locals.serviceResponse = await MyService.function('Add data args here');
+	next();
+});
+```
--- Src/Test/MonitorService.test.ts
+++ Src/Test/MonitorService.test.ts
@@ -1,5 +1,30 @@
 import { MonitorService } from '../Services';
 
-test('template', () => {
-	expect(1 + 2).toBe(3);
+function example(shouldThrow: boolean = false) {
+	if (shouldThrow) throw new Error('Example Error');
+	return true;
+}
+
+describe('Function example', () => {
+	test('should return true', () => {
+		// arrange
+		const expected: boolean = true;
+
+		// act
+		const actual = example();
+
+		// assert
+		expect(actual).toBe(expected);
+	});
+
+	describe('Error Handling', () => {
+		test('should throw Error', () => {
+			const action = () => {
+				example(true);
+			};
+
+			// assert
+			expect(action).toThrow(Error);
+		});
+	});
 });
