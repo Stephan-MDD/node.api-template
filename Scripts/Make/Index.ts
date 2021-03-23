5d59ee27090535c361b71e4f64cfd616865e390d
Stephan <stephan.mdd@gmail.com>
2021-03-23 20:08:14 +0100

    reduced file function

--- Scripts/Make/Index.ts
+++ Scripts/Make/Index.ts
@@ -7,10 +7,6 @@ const argument: string = String(args._[0]);
 const feedWidth: number = process.stdout.columns;
 const feedHeight: number = process.stdout.rows;
 
-/**
- * if file exist log warning with override command
- */
-
 if (argument === 'undefined') {
 	console.log(`${colors.red}No argument declared`, colors.reset);
 	console.log(`Documentation: ${colors.blue}yarn make ${colors.cyan}--help`, colors.reset);
@@ -23,7 +19,7 @@ const entity = argument.charAt(0).toUpperCase() + argument.slice(1).toLowerCase(
 const targets = [
 	{ directory: 'Controllers', templateSuffix: 'Controller', suffix: 'Controller' },
 	{ directory: 'Models', templateSuffix: 'Model', suffix: '' },
-	{ directory: 'Services', templateSuffix: 'Service', suffix: 'Service', option: 'importAll' },
+	{ directory: 'Services', templateSuffix: 'Service', suffix: 'Service', option: 'useDefault' },
 	{ directory: 'Test', templateSuffix: 'Test', suffix: 'Service.test', option: 'ignoreIndex' },
 ];
 
@@ -34,27 +30,35 @@ console.log();
 
 (async () => {
 	const fileTasks = targets.map(async ({ directory, suffix, templateSuffix, option }) => {
+		const filePath: string = `Src/${directory}/${entity}${suffix}.ts`;
+		const indexPath: string = `Src/${directory}/Index.ts`;
+
+		try {
+			const _bytes: Buffer = await fs.readFile(filePath);
+			// return // handle exists?
+		} catch (error) {
+			console.log("File doesn't exist");
+			// if file exist log warning with override command
+		}
+
 		try {
 			const bytes = await fs.readFile(`${__dirname}/Templates/Template${templateSuffix}`);
 			let data = bytes.toString();
 			data = data.replace(/__target__/g, entity);
 			data = data.replace(/__target_lower__/g, entity.toLowerCase());
 
-			const filePath: string = `Src/${directory}/${entity}${suffix}.ts`;
-			const indexPath: string = `Src/${directory}/Index.ts`;
 			// await fs.writeFile(filePath, data);
+		} catch (error) {
+			console.log(error);
+			// call clean function;
+			throw error;
+		}
 
-			if (option === 'ignoreIndex') return { created: filePath };
+		if (option === 'ignoreIndex') return { created: filePath };
 
-			if (option === 'importAll') {
-				const appendContent = `export * as ${entity}${suffix} from './${entity}${suffix}';`;
-
-				// await fs.appendFile(indexPath, appendContent);
-				return { created: filePath, modified: indexPath };
-			}
-
-			const appendContent = `export { default as ${entity}${suffix} } from './${entity}${suffix}';`;
-			// await fs.appendFile(indexPath, appendContent);
+		try {
+			const method = option === 'useDefault' ? `{ default as ${entity}${suffix} }` : `* as ${entity}${suffix}`;
+			// await fs.appendFile(indexPath, `export ${method} from './${entity}${suffix}';`);
 			return { created: filePath, modified: indexPath };
 		} catch (error) {
 			console.log(error);
@@ -64,7 +68,7 @@ console.log();
 
 	const res = await Promise.all(fileTasks);
 	const { created, modified } = res.reduce(
-		(acc: any, cur) => {
+		(acc: any, cur: any) => {
 			if (cur.created) acc.created.push(cur.created);
 			if (cur.modified) acc.modified.push(cur.modified);
 			return acc;
@@ -74,11 +78,11 @@ console.log();
 
 	clearInterval(loader);
 	console.log(`${colors.green}Successfully Created Resources:`, colors.reset);
-	created.forEach((item) => console.log(`${colors.cyan}• ${item}`, colors.reset));
+	created.forEach((item: string) => console.log(`${colors.cyan}• ${item}`, colors.reset));
 	console.log();
 
 	console.log(`${colors.green}Successfully Modified Index Files:`, colors.reset);
-	modified.forEach((item) => console.log(`${colors.cyan}• ${item}`, colors.reset));
+	modified.forEach((item: string) => console.log(`${colors.cyan}• ${item}`, colors.reset));
 	console.log();
 
 	console.log(`${colors.yellow}Warning:`, colors.reset);
