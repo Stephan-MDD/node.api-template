3c7fddc2d330af86e1ea5bbbd740f2dbfc643cba
Stephan <stephan.mdd@gmail.com>
2021-03-23 08:56:23 +0100

    extended template script

--- Scripts/Make/Index.ts
+++ Scripts/Make/Index.ts
@@ -19,56 +19,78 @@ const argument: string = process.argv[2]; // handle with yargs
  */
 
 if (!argument) {
-	console.log(colors.red, 'No argument declared', colors.reset);
-	console.log(colors.magenta, 'No argument declared', colors.reset);
+	console.log(`${colors.red}No argument declared`, colors.reset);
+	console.log(`documentation: ${colors.blue}yarn make ${colors.cyan}--help`, colors.reset);
 	process.exit();
 }
 
 const entity = argument.charAt(0).toUpperCase() + argument.slice(1).toLowerCase();
-// const targets: string[] = ['Controller', 'Service']; //, 'Model', 'Service', 'Test'];
 
 const targets = [
 	{ directory: 'Controllers', templateSuffix: 'Controller', suffix: 'Controller' },
-	//{ directory: 'Models', templateSuffix: 'Model', suffix: '' },
-	//{ directory: 'Services', templateSuffix: 'Service', suffix: 'Service' },
-	//{ directory: 'Test', templateSuffix: 'Test', suffix: 'Service.test' },
+	{ directory: 'Models', templateSuffix: 'Model', suffix: '' },
+	{ directory: 'Services', templateSuffix: 'Service', suffix: 'Service', option: 'importAll' },
+	{ directory: 'Test', templateSuffix: 'Test', suffix: 'Service.test', option: 'ignoreIndex' },
 ];
 
 console.clear();
 const loader = animatedLoader();
 console.log(`${colors.blue}Initiating resource creation for`, `${colors.cyan}${entity}`, colors.reset);
-
 console.log();
-console.log(`${colors.green}Successfully Created Resources:`, colors.reset);
 
 (async () => {
-	const fileTasks = targets.map(async ({ directory, suffix, templateSuffix }) => {
+	const fileTasks = targets.map(async ({ directory, suffix, templateSuffix, option }) => {
 		try {
 			const bytes = await fs.readFile(`${__dirname}/Templates/Template${templateSuffix}`);
 			let data = bytes.toString();
 			data = data.replace(/__target__/g, entity);
 			data = data.replace(/__target_lower__/g, entity.toLowerCase());
 
-			// adds Controller
-			await fs.writeFile(`Src/${directory}/${entity}${suffix}.ts`, data);
+			const filePath: string = `Src/${directory}/${entity}${suffix}.ts`;
+			const indexPath: string = `Src/${directory}/Index.ts`;
+			// await fs.writeFile(filePath, data);
+
+			if (option === 'ignoreIndex') return { created: filePath };
+
+			if (option === 'importAll') {
+				const appendContent = `export * as ${entity}${suffix} from './${entity}${suffix}';`;
+
+				// await fs.appendFile(indexPath, appendContent);
+				return { created: filePath, modified: indexPath };
+			}
 
 			const appendContent = `export { default as ${entity}${suffix} } from './${entity}${suffix}';`;
-			await fs.appendFile(`Src/${directory}/Index.ts`, appendContent);
-
-			console.log(`${colors.cyan}• Src/${directory}/${entity}${suffix}.ts`, colors.reset);
+			// await fs.appendFile(indexPath, appendContent);
+			return { created: filePath, modified: indexPath };
 		} catch (error) {
 			console.log(error);
 			// call clean function;
 		}
 	});
 
-	await Promise.all(fileTasks);
+	const res = await Promise.all(fileTasks);
+	const { created, modified } = res.reduce(
+		(acc: any, cur) => {
+			if (cur.created) acc.created.push(cur.created);
+			if (cur.modified) acc.modified.push(cur.modified);
+			return acc;
+		},
+		{ created: [], modified: [] }
+	);
+
 	clearInterval(loader);
-
-	function clean() {}
-
+	console.log(`${colors.green}Successfully Created Resources:`, colors.reset);
+	created.forEach((item) => console.log(`${colors.cyan}• ${item}`, colors.reset));
 	console.log();
+
+	console.log(`${colors.green}Successfully Modified Index Files:`, colors.reset);
+	modified.forEach((item) => console.log(`${colors.cyan}• ${item}`, colors.reset));
+	console.log();
+
 	console.log(`${colors.yellow}Warning:`, colors.reset);
 	console.log(`${colors.reset}${entity}Service.ts will throw:`, `${colors.magenta}Error('Not Implemented')`, colors.reset);
 	console.log();
 })();
+
+// remove generated files
+function clean() {}
--- Src/Test/UserService.test.ts
+++ Src/Test/UserService.test.ts
@@ -2,23 +2,25 @@ import { UserService } from '../Services';
 import { HttpCodes } from '../Enums';
 import { User } from '../Models';
 
-test('Function getAll', async () => {
-	// arrange
-	const expectedData: User[] = null;
-	const expectedStatus: HttpCodes = HttpCodes.Ok;
-	const expectedMessage: string = '';
+describe('Function getAll', () => {
+	test('Should return User[]', async () => {
+		// arrange
+		const expectedData: User[] = null;
+		const expectedStatus: HttpCodes = HttpCodes.Ok;
+		const expectedMessage: string = '';
 
-	// act
-	const actual = await UserService.getAll();
+		// act
+		const actual = await UserService.getAll();
 
-	// assert
-	expect(expectedData).toBe(actual.data);
-	expect(expectedStatus).toBe(actual.status);
-	expect(expectedMessage).toBe(actual.message);
+		// assert
+		expect(expectedData).toBe(actual.data);
+		expect(expectedStatus).toBe(actual.status);
+		expect(expectedMessage).toBe(actual.message);
+	});
 });
 
 describe('Function getSingle', () => {
-	test('should return true', async () => {
+	test('Should return User', async () => {
 		// arrange
 		const userId = 1;
 
@@ -52,16 +54,16 @@ describe('Function getSingle', () => {
 });
 
 describe('Function addSingle', () => {
-	test('should return true', async () => {
+	test('Should add User', async () => {
 		// arrange
-		const userId = 1;
+		const user: User = new User();
 
 		const expectedData: User = null;
 		const expectedStatus: HttpCodes = HttpCodes.Ok;
 		const expectedMessage: string = '';
 
 		// act
-		const actual = await UserService.getSingle(userId);
+		const actual = await UserService.addSingle(user);
 
 		// assert
 		expect(expectedData).toBe(actual.data);
@@ -72,11 +74,81 @@ describe('Function addSingle', () => {
 	describe('Error Handling', () => {
 		test('should throw Error', () => {
 			// arrange
-			const userId = -1;
+			const user: User = new User();
 
 			// act
 			const action = async () => {
-				await UserService.getSingle(userId);
+				await UserService.addSingle(user);
+			};
+
+			// assert
+			expect(action).rejects.toThrow(Error);
+		});
+	});
+});
+
+describe('Function updateSingle', () => {
+	test('Should update User', async () => {
+		// arrange
+		const userId: number = 1;
+		const user: User = new User();
+
+		const expectedData: User = null;
+		const expectedStatus: HttpCodes = HttpCodes.Ok;
+		const expectedMessage: string = '';
+
+		// act
+		const actual = await UserService.updateSingle(userId, user);
+
+		// assert
+		expect(expectedData).toBe(actual.data);
+		expect(expectedStatus).toBe(actual.status);
+		expect(expectedMessage).toBe(actual.message);
+	});
+
+	describe('Error Handling', () => {
+		test('should throw Error', () => {
+			// arrange
+			const userId: number = -1;
+			const user: User = new User();
+
+			// act
+			const action = async () => {
+				await UserService.updateSingle(userId, user);
+			};
+
+			// assert
+			expect(action).rejects.toThrow(Error);
+		});
+	});
+});
+
+describe('Function deleteSingle', () => {
+	test('Should delete User', async () => {
+		// arrange
+		const userId: number = 1;
+
+		const expectedData: User = null;
+		const expectedStatus: HttpCodes = HttpCodes.Ok;
+		const expectedMessage: string = '';
+
+		// act
+		const actual = await UserService.deleteSingle(userId);
+
+		// assert
+		expect(expectedData).toBe(actual.data);
+		expect(expectedStatus).toBe(actual.status);
+		expect(expectedMessage).toBe(actual.message);
+	});
+
+	describe('Error Handling', () => {
+		test('should throw Error', () => {
+			// arrange
+			const userId: number = -1;
+
+			// act
+			const action = async () => {
+				await UserService.deleteSingle(userId);
 			};
 
 			// assert
